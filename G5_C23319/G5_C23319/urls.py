da65b9b3309e2661937a013837b2258d46360ed1
nmiguenz <nmiguenz@gmail.com>
2023-04-17 20:34:30 -0300

    Subida de la app autenticación corregida

--- G5_C23319/G5_C23319/urls.py
+++ G5_C23319/G5_C23319/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('publica.urls')),
     path('administracion/',include('administracion.urls')),
+    path('autenticacion/',include('autenticacion.urls')),
 ]
