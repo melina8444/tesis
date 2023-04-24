9c1db05ed8e444a29c39af26c3bed359aff62eb8
cecilialsantin <chikhakituti@gmail.com>
2023-04-24 20:05:39 -0300

    ajuste del nuevo template index.html en base al template de melina, agregegado de Formulario de contacto

--- G5_C23319/publica/urls.py
+++ G5_C23319/publica/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Inicio"),
-    path('login/', views.login, name="login")
+    path('login/', views.login, name="login"),
+    path('contacto/', views.contacto, name="contacto")
 ]
