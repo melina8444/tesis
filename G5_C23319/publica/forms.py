69b542254d88e105967c9bf643d45413f205bc57
cecilialsantin <chikhakituti@gmail.com>
2023-05-03 21:44:57 -0300

    agregado de formulario reservar y reservar por camping

--- G5_C23319/publica/forms.py
+++ G5_C23319/publica/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ValidationError
 import re
 
+
 def solo_caracteres(value):
     if any(char.isdigit() for char in value):
         raise ValidationError('El nombre no puede contener números. %(valor)s',
@@ -46,4 +47,15 @@ class ContactForm(forms.Form):
                                attrs={'rows':3, 'cols':100, 'class':'form-control', 'placeholder':'Ingrese un comentario o consulta'}
                             )
                            )
-
+class ReservaForm(forms.Form):
+    name = forms.CharField(label='Nombre', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control m-1','placeholder':'Tu nombre' }))
+    
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control m-1','placeholder': 'Tu email',}))
+    
+    check_in_date = forms.DateField(label='Check-in Fecha',widget=forms.DateInput(attrs={'class': 'form-control m-1','placeholder':'Ingresá la fecha de llegada',}))
+    
+    check_out_date = forms.DateField(label='Check-out Fecha',widget=forms.DateInput(attrs={'class': 'form-control m-1','placeholder':'Ingresá la fecha de partida',}))
+    
+    num_guest = forms.IntegerField(label='Número de Huéspedes',widget=forms.NumberInput(attrs={'class': 'form-control m-1','placeholder': 'Ingresá cuántas personas realizan la reserva',}))
+    
+    campsite_id = forms.IntegerField(label='Camping',required=False, widget=forms.HiddenInput())
--- G5_C23319/publica/views.py
+++ G5_C23319/publica/views.py
@@ -2,63 +2,71 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.http import HttpResponseRedirect
-from publica.forms import ContactForm
+from publica.forms import ContactForm, ReservaForm
 from django.contrib import messages
 
 
 import publica
 
 campsites_list = [
-        {'name': 'Abracal',
+        {'id': 1,
+         'name': 'Abracal',
          'location': 'San Javier - Córdoba',
          'category':'Camping Premium',
          'description':'Conservación, educación ambiental, observación de fauna silvestre, turismo',
          'image': 'Abracaral.jpeg',
          'availability': 'Enero-Marzo 2023'
          },
-          {'name': 'Achalay',
+         {'id': 2,
+         'name': 'Achalay',
          'location': 'Primera sección de islas del Delta, Tigre, Provincia de Buenos Aires',
          'category':'Camping Premium',
          'description':'Conservación, educación ambiental, turismo, investigación, observación de fauna silvestre',
          'image': 'Achalay.jpeg',
          'availability': 'Marzo-Diciembre 2023'
          },
-          {'name': 'Curindy',
+         {'id': 3,
+         'name': 'Curindy',
          'location': 'Localidad de Garuhapé, provincia de Misiones',
          'category':'Camping Premium',
          'description':'Conservación, explotación forestal, ganadería, investigación y turismo',
          'image':'Curindy.jpeg',
          'availability': 'Octubre-Diciembre 2023'
          },
-          {'name': 'Cachape',
+         {'id': 4,
+         'name': 'Cachape',
          'location': 'Localidad de General José de San Martín, provincia de Chaco',
          'category':'Camping Básico',
          'description':'Conservación compatible con turismo y producción',
          'image':'Cachape.jpeg',
          'availability': 'Junio-Noviembre 2023'
          },
-          {'name': 'Caranday',
+         {'id': 5,
+         'name': 'Caranday',
          'location': ' Villaguay, Entre Ríos',
          'category':'Camping Aventurero',
          'description':'Conservación compatible con Turismo y Producción',
          'image':'Caranday.jpeg',
          'availability': 'Octubre-Diciembre 2023'
          },
-          {'name': 'Caspinchango',
+         {'id': 5,
+         'name': 'Caspinchango',
          'location': 'Localidad de Famaillá, provincia de Tucumán',
          'category':'Camping Premium',
          'description':'Conservación, educación ambiental, producción e investigación científica',
          'image':'Caspinchango.jpeg',
          'availability': 'Marzo-Diciembre 2023'
          },
-          {'name': 'Cerro Champaquí',
+         {'id': 6,
+         'name': 'Cerro Champaquí',
          'location': 'Paraje Los Molles, Villa de las Rosas, provincia de Córdoba',
          'category':'Camping Aventurero',
          'description':'Conservación compatible con turismo y producción',
          'image':'Cerro-Champaqui.jpeg',
          'availability': 'Marzo-Diciembre 2023'
          },
-          {'name': 'El cantar de la Pachamama',
+         {'id': 7,
+         'name': 'El cantar de la Pachamama',
          'location': 'Localidad de El Soberbio, provincia de Misiones',
          'category':'Camping Premium',
          'description':'',
@@ -84,18 +92,18 @@ def login(request):
     
     return render(request,'publica/login.html')
 
-def contacto(request):
+def contact(request):
     if request.method == 'POST':
         contactForm = ContactForm(request.POST)
         # contactForm.save(); #Para guardar en la base de datos
         if contactForm.is_valid():
             messages.success(request,'Hemos recibido tus datos')
-            return HttpResponseRedirect('/contacto/')
+            return HttpResponseRedirect('/contact/')
         else:
             messages.warning(request,'Por favor revisa los errores en el formulario')
     else:
         contactForm = ContactForm()
-    return render(request, 'publica/contacto.html', {'contactform':contactForm})
+    return render(request, 'publica/contact.html', {'contactform':contactForm})
         
 def aboutus(request):
     developers_list = [
@@ -131,3 +139,29 @@ def aboutus(request):
             }
     return render(request,'publica/aboutus.html', context)
     
+def reserva(request):
+    if request.method == 'POST':
+        reservaForm = ReservaForm(request.POST)
+        # reservaForm.save(); #Para guardar en la base de datos
+        if reservaForm.is_valid():
+            messages.success(request,'Hemos recibido tus datos')
+            return HttpResponseRedirect('/reserva/')
+        else:
+            messages.warning(request,'Por favor revisa los errores en el formulario')
+    else:
+        reservaForm = ReservaForm()
+    return render(request, 'publica/reserva.html', {'reservaform':reservaForm})
+
+def reserva_camp_id(request, campsite_id):
+  
+    if request.method == 'POST':
+        reservaForm = ReservaForm(request.POST)
+        # reservaForm.save(); #Para guardar en la base de datos
+        if reservaForm.is_valid():
+            messages.success(request,'Hemos recibido tus datos')
+            return HttpResponseRedirect('/reserva/<int:campsite_id>/')
+        else:
+            messages.warning(request,'Por favor revisa los errores en el formulario')
+    else:
+        reservaForm = ReservaForm()
+    return render(request, 'publica/reserva_camp_id.html', {'reservaform':reservaForm, 'campsite_id':campsite_id})
