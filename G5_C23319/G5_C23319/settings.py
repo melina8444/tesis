82e3c7726df7b1306c44d046c8ef782ce1568dd4
Melina Yangüez <67840710+melina8444@users.noreply.github.com>
2023-11-09 19:21:00 -0300

    cambie en el  setting la bd

--- G5_C23319/G5_C23319/settings.py
+++ G5_C23319/G5_C23319/settings.py
@@ -97,9 +97,9 @@ WSGI_APPLICATION = 'G5_C23319.wsgi.application'
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
     }
-}  """
+}  """ 
   
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DB_NAME'),
@@ -108,7 +108,7 @@ DATABASES = {
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
     }
-} 
+}  """
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
